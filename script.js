8d508f5a541f4c341d275c446af6b06980fbaa86
TomHeremans <tomheremans2002@gmail.com>
2024-12-23 16:34:12 +0100

    Add Delete button

--- script.js
+++ script.js
@@ -121,4 +121,11 @@ function documentLoader() {
     }
   }
 // write another function that will toggle the display of the deletions by clicking on a button
+  function DelFunct() {
+  var del_vision = document.getElementsByTagName('del');
+  var del_Array = Array.from(del_vision);
+  del_Array.forEach((DelElement) => {DelElement.bold();});
+  }
+      
+  }
 // EXTRA: write a function that will display the text as a reading text by clicking on a button or another dropdown list, meaning that all the deletions are removed and that the additions are shown inline (not in superscript)
