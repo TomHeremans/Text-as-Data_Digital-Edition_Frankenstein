54660a8301735ed2284139eeadea694205e049c8
TomHeremans <tomheremans2002@gmail.com>
2024-12-23 14:43:45 +0100

    Update JavaScript to fix Display functions

--- script.js
+++ script.js
@@ -93,9 +93,7 @@ function documentLoader() {
     });
   }
 
-  // Initial document load
-  documentLoader();
-  statsLoader();
+
   // Event listener for sel1 change
   function selectHand(event) {
   var visible_all = document.getElementsByTagName('div').inText;
@@ -106,7 +104,8 @@ function documentLoader() {
   var PercyArray = Array.from(visible_percy);
     if (event.target.value == 'both') {
     //write an forEach() method that shows all the text written and modified by both hand (in black?). The forEach() method of Array instances executes a provided function once for each array element.
-    
+     MaryArray.forEach((MaryElement) => {MaryElement.style.color = 'black';});
+     PercyArray.forEach((PercyElement) => {PercyElement.style.color = 'black';});
     } else if (event.target.value == 'Mary') {
      //write an forEach() method that shows all the text written and modified by Mary in a different color (or highlight it) and the text by Percy in black. 
      MaryArray.forEach((MaryElement) => {MaryElement.style.color = 'crimson';});
@@ -115,8 +114,12 @@ function documentLoader() {
      MarryArray.forEach((MaryElement) => {MaryElement.style.color = 'lightgrey';});
      PercyArray.forEach((PercyElement) => {PercyElement.style.color = 'crimson';});
      //write an forEach() method that shows all the text written and modified by Percy in a different color (or highlight it) and the text by Mary in black.
-    
     }
   }
-// write another function that will toggle the display of the deletions by clicking on a button
+  
+ // Initial document load
+  documentLoader();
+  statsLoader();
+  
+  // write another function that will toggle the display of the deletions by clicking on a button
 // EXTRA: write a function that will display the text as a reading text by clicking on a button or another dropdown list, meaning that all the deletions are removed and that the additions are shown inline (not in superscript)
